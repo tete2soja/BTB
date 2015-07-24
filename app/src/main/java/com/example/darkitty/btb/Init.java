e536f806731e1ea709b88ebf892b15ac773c21d4
Darkitty <nicolaslegall34@gmail.com>
2015-07-24 10:07:24 +0200

    Add Init.java
    Add app init

---
+++ app/src/main/java/com/example/darkitty/btb/Init.java
@@ -0,0 +1,35 @@
+package com.example.darkitty.btb;
+
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by nlegall on 23/07/2015.
+ */
+public class Init extends Activity {
+    public Init() {
+        InputStream in = getResources().openRawResource(R.raw.lines);
+        File file = new File(getApplicationContext().getFilesDir(), "lines.json");
+        byte[] buff = new byte[1024];
+        int read = 0;
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            while ((read = in.read(buff)) > 0) {
+                out.write(buff, 0, read);
+            }
+            in.close();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
